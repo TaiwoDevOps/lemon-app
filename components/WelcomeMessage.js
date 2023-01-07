fe770ff508419ce1c7037a11517e2dafa52f7942
Adisa Taiwo Joshua <adisataiwojoshua@gmail.com>
2023-01-07 09:39:50 +0100

    added new components-sectionlist

--- components/WelcomeMessage.js
+++ components/WelcomeMessage.js
@@ -1,7 +1,9 @@
 import * as React from "react";
-import { Text, StyleSheet, ScrollView } from "react-native";
+import { Text, StyleSheet, ScrollView, TextInput } from "react-native";
 
 export default function WelcomeMessage() {
+  const [value, onChangeValue] = useState("");
+
   return (
     <ScrollView indicatorStyle="white" style={styles.container}>
       <Text style={styles.title}>Welcome to Little Lemon</Text>
@@ -11,25 +13,39 @@ export default function WelcomeMessage() {
         and classic cocktails in a lively but casual environment. We would love
         to hear more about your experience with us!
       </Text>
+      <TextInput
+        style={styles.input}
+        value={value}
+        onChangeText={onChangeValue}
+      />
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  title: {
+  container: {
+    flex: 1,
+  },
+  headerText: {
     padding: 40,
     fontSize: 30,
     color: "#EDEFEE",
     textAlign: "center",
   },
-  subTitle: {
-    fontSize: 38,
+  regularText: {
+    fontSize: 24,
+    padding: 20,
+    marginVertical: 8,
     color: "#EDEFEE",
     textAlign: "center",
-    marginVertical: 8,
-    padding: 20,
   },
-  container: {
-    flex: 1,
+  inputBox: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 16,
+    borderColor: "EDEFEE",
+    backgroundColor: "#EDEFEE",
   },
 });
